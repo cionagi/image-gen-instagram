e810600ced335053c3b88e9e6a767a4071110de0
Luis Zavala <>
2020-04-12 16:56:13 -0400

    add: tests config

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 function App() {
-  return <div className="container" />;
+  return (
+    <div className="container">
+      <h1>Welcome Image generator instagram</h1>
+    </div>
+  );
 }
 
 export default App;
